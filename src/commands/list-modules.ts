1ef0cfb6c789f49e959535ab0729d7ffc9e8c35c
Виктор Тяжельников <vtyazhelnikov@swaxis.com>
2020-06-05 19:42:45 +0600

    list-modules outputs modules in walk order

--- src/commands/list-modules.ts
+++ src/commands/list-modules.ts
@@ -1,17 +1,20 @@
 import { getProject } from "../project";
 import * as columnify from "columnify";
 import { getRegistryForPackage } from "../registry-paths";
+import { walkAllLocalModules } from "../deps/dry-dependency-tree";
 
 
 export async function listModulesCommand() {
-  const config = getProject();
+  let data: any[] = [];
 
-  const data = config.modules.map(module => ({
-    name: module.name ? module.name.name : "<no name>",
-    path: module.path,
-    registry: getRegistryForPackage(module.checkedName),
-    useNpm: module.useNpm
-  }));
+  await walkAllLocalModules(async module => {
+    data.push({
+      name: module.name ? module.name.name : "<no name>",
+      path: module.path,
+      registry: getRegistryForPackage(module.checkedName),
+      useNpm: module.useNpm
+    });
+  });
 
   console.log(columnify(data, {
     columnSplitter: " | "
