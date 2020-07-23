7120000762c4d31b30239daf38207c7e5d6303a8
Виктор Тяжельников <vtyazhelnikov@swaxis.com>
2020-07-23 18:28:02 +0600

    fix `rollman server` command

--- src/commands/server.ts
+++ src/commands/server.ts
@@ -2,9 +2,14 @@ import * as chalk from "chalk";
 import { NpmRegistry } from "../registry";
 import { getConfig, RegistryServerType } from "../config/config";
 import { shutdown } from "../shutdown";
+import * as events from "events";
 
 
-export async function startServerCommand() {
+class Piggy extends events.EventEmitter {
+}
+
+
+export async function startServerCommand(): Promise<void> {
   let config = getConfig();
 
   if (config.registryServerType !== RegistryServerType.ManagedLocal) {
@@ -13,4 +18,12 @@ export async function startServerCommand() {
   }
 
   await NpmRegistry.init();
+
+  return new Promise(resolve => {
+    let piggy = new Piggy();
+    piggy.on("fly", () => {
+      // we should never return, NpmRegistry class process.exit when server closes
+      resolve();
+    });
+  });
 }
