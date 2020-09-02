8e353f4f9cf38894837cd3e71a8cd84756f6d936
Виктор Тяжельников <vtyazhelnikov@swaxis.com>
2020-09-02 17:48:06 +0600

    update lockfile after running npm command

--- src/commands/npm.ts
+++ src/commands/npm.ts
@@ -3,6 +3,10 @@ import { getProject } from "../project";
 import { NpmRunner } from "../module-npm-runner";
 import { NpmRegistry } from "../registry";
 import { getConfig } from "../config/config";
+import { Lockfile } from "../lockfile";
+
+
+const CAN_CHANGE_LOCKFILE = [ "i", "install", "add", "link", "ln", "audit", "r", "rm", "remove", "un", "unlink", "uninstall", "prune", "update", "up", "upgrade" ];
 
 
 export async function npmCommand() {
@@ -18,5 +22,12 @@ export async function npmCommand() {
   let project = getProject();
   let dir = process.cwd();
   let mod = project.modules.find(module => module.path === dir);
-  await NpmRunner.run(mod, args.args);
+
+  let npmArgs = args.args;
+
+  await NpmRunner.run(mod, npmArgs);
+
+  if (mod && npmArgs.length > 1 && CAN_CHANGE_LOCKFILE.includes(npmArgs[0]) && Lockfile.existsInModule(mod)) {
+    Lockfile.forModule(mod).update();
+  }
 }
