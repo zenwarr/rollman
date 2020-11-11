7f7eb14a120f73576b70f13a824aa2f4d224abdf
Виктор Тяжельников <vtyazhelnikov@swaxis.com>
2020-11-11 16:01:26 +0600

    feat: support running arbitrary commands in `rollman each`

--- src/commands/each.ts
+++ src/commands/each.ts
@@ -11,20 +11,31 @@ export async function eachCommand() {
   assert(args.subCommand === "each");
 
   const scriptName = args.args[0];
-
-  await walkModules(async mod => {
-    if (!mod.useNpm) {
-      return;
+  if (scriptName === "--") {
+    if (args.args.length < 2) {
+      throw new Error(`Not enough arguments: expected at least one after --`);
     }
 
-    const manifest = getManifestManager().readPackageManifest(mod.path);
-    if (!(scriptName in manifest.scripts)) {
-      console.log(`Script ${ scriptName } not present in module ${ mod.checkedName.name }, skipping`);
-      return;
-    }
-
-    await runCommand(getYarnExecutable(), args.args, {
-      cwd: mod.path
+    await walkModules(async mod => {
+      await runCommand(args.args[1], args.args.slice(2), {
+        cwd: mod.path
+      });
     });
-  });
+  } else {
+    await walkModules(async mod => {
+      if (!mod.useNpm) {
+        return;
+      }
+
+      const manifest = getManifestManager().readPackageManifest(mod.path);
+      if (!(scriptName in manifest.scripts)) {
+        console.log(`Script ${ scriptName } not present in module ${ mod.checkedName.name }, skipping`);
+        return;
+      }
+
+      await runCommand(getYarnExecutable(), args.args, {
+        cwd: mod.path
+      });
+    });
+  }
 }
